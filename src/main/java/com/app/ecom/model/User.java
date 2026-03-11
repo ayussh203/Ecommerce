f0a1c3276c700437f3045f589f14287fd24dfb80
Ayush Srivastava <ayush200srivastava2003@gmail.com>
2026-03-11 20:03:19 +0530

    Defining Address Entity and Relationship with User

--- src/main/java/com/app/ecom/model/User.java
+++ src/main/java/com/app/ecom/model/User.java
@@ -4,6 +4,10 @@ package com.app.ecom.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
@@ -19,5 +23,15 @@ public class User {
     private String email;
     private String phone;
     private UserRole role=UserRole.CUSTOMER;
+
+    @OneToOne(cascade = CascadeType.ALL,orphanRemoval = true)
+    @JoinColumn(name="address_id",referencedColumnName = "id")
+    private Address address;
+
+    @CreationTimestamp
+    private LocalDate createdAt;
+
+    @UpdateTimestamp
+    private LocalDate updatedAt;
 
 }
